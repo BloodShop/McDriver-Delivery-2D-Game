d2ba69feef911fde4bea885532e143c5e718ef93
BloodShop <koliakovcr7@gmail.com>
2022-06-21 10:29:22 +0300

    McDriver Delivery 2D Game
    McDriver Delivery 2D simple game adding several driving logics, triggers and collisions.

---
+++ McDriver/Assets/Delivery.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    [SerializeField] Color32 hasPackageColor = new Color32(60, 255, 0, 255);
+    [SerializeField] Color32 noPackageColor = new Color32(255, 255, 255, 255);
+    [SerializeField] float destroyDelay = 0.15f;
+    bool hasPackage;
+    SpriteRenderer spriteRenderer;
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        Debug.Log("Ouch!");
+    }
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Package" && !hasPackage) {
+            Debug.Log("Package is picked");
+            hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(other.gameObject, destroyDelay);
+        }
+        
+        if (other.tag == "Customer" && hasPackage) {
+            Debug.Log("Package delivered");
+            hasPackage = false;
+            spriteRenderer.color = noPackageColor;
+        }
+    }
+}
